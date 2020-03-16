7ad0c59544b240b3732cfe28ecb77f63d3b7cfdd
chanin <pci2676@gmail.com>
2020-03-16 14:48:21 +0900

    init

---
+++ src/test/java/com/woowacourse/woowaquiz/WoowaquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.woowaquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowaquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
