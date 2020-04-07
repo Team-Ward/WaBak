94ae417970eec85458edc90c0af2a138c70f79cf
chanin <pci2676@gmail.com>
2020-04-08 01:47:40 +0900

    feat : web module 생성

---
+++ woowa-quiz/src/test/java/com/woowacourse/woowascrum/WoowaScrumApplicationTests.java
@@ -0,0 +1,15 @@
+package com.woowacourse.woowascrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+class WoowaScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- woowa-web/src/main/java/com/woowacourse/woowascrum/WoowaScrumApplication.java
+++ woowa-web/src/main/java/com/woowacourse/woowascrum/WoowaScrumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WoowaquizApplication {
+public class WoowaScrumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WoowaquizApplication.class, args);
+		SpringApplication.run(WoowaScrumApplication.class, args);
 	}
 
 }
