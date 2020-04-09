875cf2ce997f2996a1383c81509d3a68027529ba
chanin <pci2676@gmail.com>
2020-04-09 20:05:36 +0900

    refactor: 불필요 테스트 제거

--- woowa-quiz/src/test/java/com/woowacourse/woowascrum/WoowaScrumApplicationTests.java
+++ woowa-quiz/src/test/java/com/woowacourse/woowascrum/WoowaScrumApplicationTests.java
@@ -1,15 +1,9 @@
 package com.woowacourse.woowascrum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 class WoowaScrumApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
